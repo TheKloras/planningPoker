1db99c45cb4911220970f5507f2ced33baff1e42
ErnestJasa <ernestjasaitis@gmail.com>
2023-02-17 16:33:29 +0200

    api

---
+++ PlanningPokerAPI/PlanningPokerAPI/Interfaces/IGuestUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using PlanningPokerAPI.Models;
+
+namespace PlanningPokerAPI.Interfaces
+{
+    public interface IGuestUserRepository
+    {
+        GuestUser Create(GuestUser guestUser);
+        bool Delete(string name);
+
+        bool Save();
+    }
+}
